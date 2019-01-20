21d6a7a0a35694450d40c0e8abb1ebe1231e586d
seen2 <tiwarietc@gmail.com>
2019-01-21 00:29:19 +0530

    object concepts

---
+++ Pencil/src/Pen.java
@@ -0,0 +1,18 @@
+public class Pen {
+	
+	public int length;
+	private String color="blue"; 
+	public float diameter;
+	
+	public static long nextId; 
+	
+	public void setColor(String myColor) {
+		color=myColor;
+	}
+	
+	public String getColor() {
+		return color;
+		
+	}
+
+}
