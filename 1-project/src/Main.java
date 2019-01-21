608f1196edacd61b2889043d4799179a23874d4c
seen2 <tiwarietc@gmail.com>
2019-01-21 23:23:48 +0530

    use of static keyword

--- 1-project/src/Main.java
+++ 1-project/src/Main.java
@@ -9,12 +9,16 @@ public class Main {
 		//Tempreture three=new Tempreture('C');
 		//Tempreture four=new Tempreture(val,'C');
 		Tempreture n=new Tempreture(val,'F');
-		System.out.println(one.getTempreture());
-		System.out.println(n.getTempreture());
+		//System.out.println(one.getTempreture());
+		//System.out.println(n.getTempreture());
+		
 		
 		//System.out.println(n.getTempreture());
-		//System.out.println(n.tempratureC(-40));
-		System.out.println(one.compareTempreture(n));
+		System.out.println(n.tempratureC(-40));
+		/*
+		//using static function via class name
+		System.out.println(Tempreture.compareTempretures(one,n));
+		*/
 		//System.out.println(n.getTempreture());
 	}
 
--- 1-project/src/Tempreture.java
+++ 1-project/src/Tempreture.java
@@ -1,6 +1,5 @@
 
 public class Tempreture {
-	
 	private float value;
 	private char scale;
 	
@@ -27,49 +26,39 @@ public class Tempreture {
 		scale=newScale;
 		value=newValue;
 	}
-	
-	
+	//temperature converters
 	public float  tempratureC(float f){
 		if(this.scale=='F') {
-			float newValue=5*(f-32)/9;
-			
+			float newValue=5*(f-32)/9;	
 			return (newValue);
 		}
 		else {
 			return (f);
 		}
-		
 	}
 	public float tempratureF(float c){
-		
 		if(this.scale=='C') {
-			float newValue=9*(c)/5+32;
-			
+			float newValue=9*(c)/5+32;	
 			return (newValue);
 		}
 		else {
 			return (c);
-		}
-			
+		}	
 	}
-	
+	//modifies or setters
 	public void setValue(float newValue) {
 		this.value=newValue;
 		
 	}
 	public void setScale(char newScale) {
 		this.scale=newScale;
-		
 	}
 	public void setTempreture(float newValue,char newScale) {
 		this.value=newValue;
 		this.scale=newScale;
-		
 	}
 	public int compareTempreture(Tempreture t) {
-		
-		if((this.scale=='F' && t.scale=='F')|| (this.scale=='C' && t.scale=='C')) {
-			
+		if((this.scale=='F' && t.scale=='F')|| (this.scale=='C' && t.scale=='C')) {	
 			if(this.value==t.value) {
 				return 0;
 			}
@@ -81,7 +70,6 @@ public class Tempreture {
 			}
 		}
 		else {
-			
 			if(this.scale!=t.scale) {
 				float value1=tempratureC(this.value);
 				float value2=tempratureC(t.value);
@@ -97,10 +85,51 @@ public class Tempreture {
 			}
 		}
 		return -1;
-		
 	}
+	
 	public String getTempreture() {
 		return Float.toString(this.value)+this.scale	;
 	}
-
+	/*
+	//use of static keyword
+	public static int compareTempretures(Tempreture t,Tempreture T) {
+		if((T.scale=='F' && t.scale=='F')|| (T.scale=='C' && t.scale=='C')) {	
+			if(T.value==t.value) {
+				return 0;
+			}
+			else if(T.value>t.value) {
+				return 1;
+			}
+			else {
+				return -1;
+			}
+		}
+		else {
+			if(T.scale!=t.scale) {
+				float value1=TempratureC(T);
+				float value2=TempratureC(t);
+				if(value1==value2) {
+					return 0;
+				}
+				else if(value1>value2) {
+					return 1;
+				}
+				else {
+					return -1;
+				}
+			}
+		}
+		return -1;
+	}
+	public static float  TempratureC(Tempreture t){
+		if(t.scale=='F') {
+			float newValue=5*(t.value-32)/9;	
+			return (newValue);
+		}
+		else {
+			return (t.value);
+		}
+	}
+	
+*/
 }
