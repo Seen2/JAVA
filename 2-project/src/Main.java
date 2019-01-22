4d5460b1c9126ec56c39457b8b9d96cbf4f770c2
seen2 <tiwarietc@gmail.com>
2019-01-22 09:20:12 +0530

    basic setup for assignment two with main

---
+++ 2-project/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Dino dOne=new Dino();
+		System.out.println(dOne.getName());
+		dOne.setName("Genda");
+		System.out.println(dOne.getName());
+		
+		Dino sOne=new Dino();
+		System.out.println(sOne.getName());
+		sOne.setName("Senda");
+		System.out.println(sOne.getName());
+
+	}
+
+}
