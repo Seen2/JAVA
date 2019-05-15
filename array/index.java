cde86ea15bb21b4c0288bf32125ba146140f6f58
seen2 <tiwarietc@gmail.com>
2019-05-16 00:36:33 +0530

    Arrays class in-built func

--- array/index.java
+++ array/index.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.*;
 public class index{
   public static void main(String []args){
     int a[]={1,3,0,4};
@@ -9,10 +9,34 @@ public class index{
     //   System.out.println(e);
     // }
 
-    System.out.println(Arrays.toString(a));
-    Arrays.sort(a);
-    System.out.println(Arrays.toString(a));
+    // System.out.println(Arrays.toString(a));
+    // Arrays.sort(a);
+    // System.out.println(Arrays.toString(a));
     Arrays.asList(a);
     System.out.println(Arrays.toString(a));
+    //binary search on array
+    System.out.println("index of 3 is:"+Arrays.binarySearch(a,3));
+
+    int []b={1,3,0,4};
+    Arrays.sort(b);
+    System.out.println("compare is:"+Arrays.equals(a,b));
+    //copy
+    b=Arrays.copyOf(a,a.length);
+    System.out.println("compare is:"+Arrays.equals(a,b));
+
+    //copy in range
+    b=Arrays.copyOfRange(a,0,(a.length-2));
+    System.out.println(Arrays.toString(b));
+
+    //deep equals
+    int c[][]={a,b},d[][]={b,a};
+    System.out.println("deep compare is:"+Arrays.deepEquals(c,d));
+    //deep tostring
+    System.out.println(Arrays.deepToString(c));
+
+    //fill
+    Arrays.fill(c,a);
+    System.out.println(Arrays.deepToString(c));
+
   }
 }
