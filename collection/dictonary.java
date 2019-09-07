2775f3a65fae95fe624d8e35a00edc24d82a250e
seen2 <tiwarietc@gmail.com>
2019-09-07 20:27:11 +0530

    useful data structure

--- collection/dictonary.java
+++ collection/dictonary.java
@@ -9,5 +9,20 @@ class dictonary {
 		System.out.println(dict);
 		System.out.println(dict.keySet());
 		System.out.println(dict.get("key"));
+		dict.put("0-key", "0-value");
+		dict.put("1-key", "1-value");
+		dict.put("2-key", "2-value");
+		dict.put("3-key", "3-value");
+		dict.put("4-key", "4-value");
+		System.out.println(dict);
+		dict.remove("2-key");
+		System.out.println(dict);
+		dict.replace("key", "value", "Value");
+		System.out.println(dict.isEmpty());
+		System.out.println(dict);
+		dict.clear();
+		System.out.println(dict);
+		System.out.println(dict.isEmpty());
+
 	}
 }
