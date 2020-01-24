d38166cc64c959a43a2f1bc0c7149ac5d3dd5fe3
seen2 <tiwarietc@gmail.com>
2020-01-24 16:42:01 +0530

    lambda expressions built in func

--- lambdaExpression/builtInFuncInterface.java
+++ lambdaExpression/builtInFuncInterface.java
@@ -1,12 +1,30 @@
 import java.io.*;
 import java.lang.*;
-import java.util.function.*;
- 
+import java.util.function.*; 
 
 class builtInFuncInterface{
 	public static void main(String args[]){
-		Consumer<Integer> c=(t)->System.out.println("hello");
+		Consumer<Integer> c=(t)->System.out.println("hello, "+t);
 		c.accept(2);
+
+		Function<Integer,String> f=(num)-> {
+			return "Square of "+num+"="+(num*num);
+		};
+		System.out.println(f.apply(3));
+
+		Predicate<Integer > p=num->{
+			return num>=0;
+		};
+
+		System.out.println(p.test(-5)?"Positive":"Negative");
+
+		Supplier<String> s=()->{
+
+			return "I am Supplier";
+		};
+
+		System.out.println(s.get());
+
 	}
 
 }
